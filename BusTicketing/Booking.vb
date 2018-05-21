d8ba6e13bac3d96c027c4d326a0cfbe604a5cb7a
mtotodev05 <mtotodev05@gmail.com>
2018-05-21 17:19:34 +0300

    updated maps

--- BusTicketing/Booking.vb
+++ BusTicketing/Booking.vb
@@ -1,4 +1,6 @@
-﻿Public Class Booking
+﻿Imports BusTicketing.MapsController
+Public Class Booking
+    Dim mapsController As New MapsController
     Private Sub Booking_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CmbDepature.Items.Add("05:00AM")
         CmbDepature.Items.Add("07:00AM")
@@ -6,5 +8,15 @@
         CmbDepature.Items.Add("03:30PM")
         CmbDepature.Items.Add("05:00PM")
         CmbDepature.Items.Add("07:00PM")
+    End Sub
+
+    Private Sub BtnBook_Click(sender As Object, e As EventArgs) Handles BtnBook.Click
+        Dim Destination As String
+        Destination = TxtDestination.Text
+        Try
+            WebMaps.Navigate(mapsController.GenerateMaps(Destination))
+        Catch ex As Exception
+            MessageBox.Show("Error in Click, Rendering Maps" + ex.Message)
+        End Try
     End Sub
 End Class
--- BusTicketing/Controller/MapsController.vb
+++ BusTicketing/Controller/MapsController.vb
@@ -1,3 +1,17 @@
-﻿Public Class MapsController
-
+﻿Imports System.Text
+Public Class MapsController
+    Dim WebQuery As New StringBuilder
+    Public Function GenerateMaps(ByVal Destination As String) As String
+        Try
+            WebQuery.Append("http://maps.google.com/maps?q=")
+            If Destination <> String.Empty Then
+                WebQuery.Append(Destination + "," & "+")
+            Else
+                MessageBox.Show("Destination Cannot be Empty")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error Rendering Your Destination " + ex.Message)
+        End Try
+        Return WebQuery.ToString
+    End Function
 End Class
